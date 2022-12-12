4bbb168bfa1d1ca3ce6dacb02a83f2f4aa60f622
josefellipe <joseserqueira@id.uff.br>
2022-12-11 21:43:52 -0300

    teste token

--- application.py
+++ application.py
@@ -1,15 +1,18 @@
 from flask import Flask, request, jsonify
+import sqlite3
 import requests
 
 application = Flask(__name__)
+
 
 @application.route('/')
 def hello_world():
     return "Hello World"
 
-@application.route('/teste', methods=['GET'])
-def whats(mensagem2):
-    return mensagem2['hub_challenge']
+@application.route('/teste', methods=['POST'])
+def whats():
+    mensagem = request.json
+    return mensagem['hub_challenge']
 
 if __name__=='__main__':
     application.run()
