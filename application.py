c2ac357c007f3b9ed493da4811266bf12d0d4420
josefellipe <joseserqueira@id.uff.br>
2022-12-11 21:52:48 -0300

    gravando no db

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import sqlite3
 import requests
 
 application = Flask(__name__)
-
+conexao = sqlite3.connect('testes-aws.db')
 
 @application.route('/')
 def hello_world():
@@ -12,6 +12,10 @@ def hello_world():
 @application.route('/teste', methods=['POST'])
 def whats():
     mensagem = request.json
+    cursor = conexao.cursor()
+    comando = f'INSERT INTO retorno (mensagem) values ("{mensagem}")'
+    cursor(comando)
+    conexao.commit()
     return mensagem['hub_challenge']
 
 if __name__=='__main__':
