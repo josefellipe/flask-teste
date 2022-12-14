0a6982a2752e769a64ca27657551a46deb9adcd3
josefellipe <joseserqueira@id.uff.br>
2022-12-14 08:09:28 -0300

    visualizar

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import sqlite3
 import requests
 
 application = Flask(__name__)
-conexao = sqlite3.connect('testes-aws.db')
+
 
 @application.route('/')
 def hello_world():
@@ -11,13 +11,26 @@ def hello_world():
 
 @application.route('/teste', methods=['POST'])
 def whats():
+    conexao = sqlite3.connect('testes-aws.db')
     mensagem = request.json
     cursor = conexao.cursor()
     comando = f'INSERT INTO retorno (mensagem) VALUES ("{mensagem}")'
     cursor.execute(comando)
     conexao.commit()
     cursor.close()
+    conexao.close()
     return mensagem['hub_challenge']
+
+@application.route('/consulta', methods=['GET'])
+def consultar():
+    conexao = sqlite3.connect('testes-aws.db')
+    cursor = conexao.cursor()
+    comando = "SELECT * FROM retorno"
+    cursor.execute(comando)
+    mensagem = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return mensagem
 
 if __name__=='__main__':
     application.run()
