e3892a4f52ab64efe3cd0667c886097a3cc44cca
josefellipe <joseserqueira@id.uff.br>
2022-12-14 08:37:17 -0300

    mudança na request

--- application.py
+++ application.py
@@ -11,8 +11,25 @@ def hello_world():
 
 @application.route('/teste')
 def whats():
-    conexao = sqlite3.connect('testes-aws.db')
-    mensagem = request.json
+    url = 'https://graph.facebook.com/v15.0/105066285771857/messages'
+    headers = {
+        'Authorization' : 'Bearer EAAHdsXNAVH8BAKeABvKvsbkBVg417GKmwgtHO3FcbODOQmlGaC8SdlwJ7bWE0YdQrpU5zqZB7ANV5YhtzBkmtpeUonilcET8jjFCU7210kQEeJtsDxI9iMCFrCqioJhPMg9PHbkD3c2J219H4zC5tBvvM4lZBOtFfDcKxN4IpbHqgevBu4U4WZCDmlZANUt1S3Nn1PFa1J5ARU0A3vQR6Iw63AC8uCUZD',
+        'Content-Type': 'application/json'
+    }
+    body = { 
+        "messaging_product": "whatsapp", 
+        "to": "", 
+        "type": "template", 
+        "template": { 
+            "name": "hello_world", 
+            "language": { 
+                "code": "en_US" 
+            } 
+        } 
+    }
+    mensagem = requests.post(url=url, headers=headers, json=body)
+
+    conexao = sqlite3.connect('testes-aws.db')  
     cursor = conexao.cursor()
     comando = f'INSERT INTO retorno (mensagem) VALUES ("{mensagem}")'
     cursor.execute(comando)
